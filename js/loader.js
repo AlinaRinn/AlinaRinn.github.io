e3b00b4441c990f22ba41345eb300af201458f9f
AlinaRinn <nik17042001@rambler.ru>
2024-05-12 02:09:53 +0300

    some css changes

--- js/loader.js
+++ js/loader.js
@@ -1,25 +1,19 @@
-var headerText = 
-`<div id="name">
-    <a href="/index.html">
-        <div id="nametxt">GRAND THEFT KOROVAN</div>
-    </a>
-</div>
-<nav>
-    <a href="/index.html">Главная</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/leaderboard.html">Лидеры набигания</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/about.html">О Корованах</a>
-</nav>`;
-
-var footerText = 
-`<p> Grand Theft Korovan 2022&nbsp;&nbsp; &copy; &nbsp;&nbsp;All rights stealed </p>`;
-
 function setHeader() {
   var header = document.createElement("header");
-  header.innerHTML = headerText;
+  header.innerHTML = `<div id="name">
+  <a href="/index.html">
+      <div id="nametxt">GRAND THEFT KOROVAN</div>
+  </a>
+</div>
+<nav>
+  <a href="/index.html">Главная</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/leaderboard.html">Лидеры набигания</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/about.html">О Корованах</a>
+</nav>`;
   document.body.insertAdjacentElement('afterbegin', header);
 }
 
 function setFooter() {
     var footer = document.createElement("footer");
-    footer.innerHTML = footerText;
+    footer.innerHTML = `<p> Grand Theft Korovan 2022&nbsp;&nbsp; &copy; &nbsp;&nbsp;All rights stealed </p>`;
     document.body.insertAdjacentElement('beforeend', footer);
 }
 
